5a184673db4ce2618ec3499cc9a352514b0f27a6
Jibril Nikki <jibrilnikki.g@gmail.com>
2026-02-12 14:54:59 +0700

    Update output file path in crawl_day.sh

--- scripts/crawl_day.sh
+++ scripts/crawl_day.sh
@@ -25,7 +25,7 @@ while true; do
     break
   fi
 
-  fname="${OUT_DIR}/tweets_${SINCE_DATE}_w${i}.csv"
+  fname="tweets-data/output/tweets_${SINCE_DATE}_w${i}.csv"
 
   echo "Fetching window ${window_start} -> ${window_end}"
 
